bd1e9f8b4849d0145a31935205a42f2e4943fdd8
ianmuriuki01 <ianmuriuki01@gmail.com>
2025-08-20 23:11:45 +0300

    NFT collection with a white list

--- src/CryptoDevs.sol
+++ src/CryptoDevs.sol
@@ -5,13 +5,12 @@ import {ERC721Enumerable, ERC721} from "@openzeppelin/contracts/token/ERC721/ext
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import "./Whitelist.sol";
 
-
-contract CryptoDevs is ERC721Enumerable, Ownable{
+contract CryptoDevs is ERC721Enumerable, Ownable {
     //cost of one Crypto Dev NFT
-    uint256 constant public _price = 0.01 ether;
+    uint256 public constant _price = 0.01 ether;
 
     //max number of crypto that can ever exist
-    uint256 constant public maxTokenIds = 20;
+    uint256 public constant maxTokenIds = 20;
 
     //Whitelist instance
     Whitelist whitelist;
@@ -20,7 +19,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable{
     uint256 public reservedTokens;
     uint256 reservedTokensClaimed = 0;
 
-    constructor (address whitelistContract) ERC721("Crypto Devs", "CD") Ownable(msg.sender){
+    constructor(address whitelistContract) ERC721("Crypto Devs", "CD") Ownable(msg.sender) {
         whitelist = Whitelist(whitelistContract);
         reservedTokens = whitelist.maxWhitelistedAddresses();
     }
@@ -29,12 +28,11 @@ contract CryptoDevs is ERC721Enumerable, Ownable{
         //make sure we always leave  enough room for whitelist reservations
         require(totalSupply() + reservedTokens - reservedTokensClaimed < maxTokenIds, "EXCEEDED_MAX_SUPPLY");
         //if user is part of the whitelist, make sure there is still reserved tokens left
-        if (whitelist.whitelistedAddresses(msg.sender) && msg.value < _price){
+        if (whitelist.whitelistedAddresses(msg.sender) && msg.value < _price) {
             //make sure user does not already own an NFT
-            require(balanceOf(msg.sender)==0, "ALREADY_OWNED");
+            require(balanceOf(msg.sender) == 0, "ALREADY_OWNED");
             reservedTokensClaimed += 1;
-        }
-        else{
+        } else {
             //if user is not part of the whitelist, make sure they have sent enough ETH
             require(msg.value >= _price, "NOT_ENOUGH_ETHER");
         }
@@ -45,9 +43,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable{
     function withdraw() public onlyOwner {
         address _owner = owner();
         uint256 amount = address(this).balance;
-        (bool sent, ) = _owner.call{value: amount}("");
+        (bool sent,) = _owner.call{value: amount}("");
         require(sent, "Failed to send Ether");
     }
-
-
 }
